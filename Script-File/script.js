601b3e7b136090dcb866951929f2a2ee811e5e52
SazzadFs <sazzadssd512@gmail.com>
2022-08-21 15:03:54 +0600

    first commit

---
+++ Script-File/script.js
@@ -0,0 +1,8 @@
+const nav = document.getElementById('nav-display');
+document.getElementById('display').addEventListener('click', function () {
+
+    nav.style.display = 'block';
+})
+document.getElementById('xross').addEventListener('click', function () {
+    nav.style.display = 'none';
+})
